561bf230b9c2c64faa7ff5673fa0895dd1eec60e
hecto <hecto@LAPTOP-D7NNA3EN>
2025-11-18 12:15:43 -0500

    feat(setup): creo un comentario prueba

--- InventarioApp/src/controller/Inventariocontroller.java
+++ InventarioApp/src/controller/Inventariocontroller.java
@@ -101,5 +101,6 @@ public class Inventariocontroller {
         } else {
             vista.mostrarMensaje("ERROR: No se encontró ningún producto con el SKU: " + sku + " para eliminar.");
         }
+        //prueba
     }
 }
